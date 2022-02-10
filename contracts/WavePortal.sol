67106a74642e131423b4e1ff0e1ad60896966856
haile <52631736+hailelagi@users.noreply.github.com>
2022-02-10 23:52:36 +0100

    feat: store state

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,19 @@ pragma solidity ^0.8.4;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint256 totalWaves;
+
     constructor() {
         console.log("lol smart contract go vroom!");
+    }
+
+    function wave() public {
+        totalWaves += 1;
+        console.log("%s ey buddy", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("%d total", totalWaves);
+        return totalWaves;
     }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,8 +1,24 @@
 async function main(){
+    const [owner, randomPerson] = await hre.ethers.getSigners()
     const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
     const waveContract = await waveContractFactory.deploy();
     await waveContract.deployed();
-    console.log("contract deployed =>", waveContract.address);
+
+    console.log("contract deployed to adress =>", waveContract.address);
+    console.log("contract deployed (owner)=>", owner.address);
+
+    let waveCount;
+    waveCount = await waveContract.getTotalWaves();
+
+    let waveTxn = await waveContract.wave();
+    await waveTxn.wait();
+
+    waveCount = await  waveContract.getTotalWaves()
+
+    waveTx = await waveContract.connect(randomPerson).wave();
+    await waveTxn.wait();
+
+    waveCount = await waveContract.getTotalWaves();
 }
 
 async function runMain() {
@@ -13,6 +29,6 @@ async function runMain() {
         console.log(error);
         process.exit(1);
     }
-};
+}
 
 runMain();
