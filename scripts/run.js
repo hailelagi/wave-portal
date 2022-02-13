5f441d05b6f0a23c84ba16f5077587e195883e3f
haile <52631736+hailelagi@users.noreply.github.com>
2022-02-13 01:35:23 +0100

    test: send ether to waver

--- scripts/run.js
+++ scripts/run.js
@@ -1,11 +1,21 @@
 async function main(){
     const [owner, randomPerson] = await hre.ethers.getSigners()
     const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
-    const waveContract = await waveContractFactory.deploy();
+    const waveContract = await waveContractFactory.deploy(
+        {value: hre.ethers.utils.parseEther("0.1")}
+    );
     await waveContract.deployed();
 
     console.log("contract deployed to address =>", waveContract.address);
     console.log("contract deployed (owner) =>", owner.address);
+
+    let contractBalance = await hre.ethers.provider.getBalance(
+        waveContract.address
+    );
+    console.log(
+        "Initial bal",
+        hre.ethers.utils.formatEther(contractBalance)
+    );
 
     let waveCount;
     waveCount = await waveContract.getTotalWaves();
@@ -14,8 +24,18 @@ async function main(){
     let waveTxn = await waveContract.wave("Hi hi test!");
     await waveTxn.wait();
 
+
     waveTxn = await waveContract.connect(randomPerson).wave(":0 test twooo!");
     await waveTxn.wait();
+
+    contractBalance = await hre.ethers.provider.getBalance(
+        waveContract.address
+    );
+
+    console.log(
+        "Last bal after two tx",
+        hre.ethers.utils.formatEther(contractBalance)
+    );
 
     let allWaves = await waveContract.getAllWaves();
 
